a97421a150046ca7098ed7ecd1567690726241d4
renaldydwij <renaldydjulianto@gmail.com>
2021-05-22 08:57:08 +0700

    intitial commit

---
+++ resources/views/template/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Starter</title>
+  
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
